2dd5c3fa84a00b6ca4bcffcfcee9cfeae23295a5
Jacob Krogh Klein <jacob_kk@otmail.com>
2014-06-09 11:54:06 +0200

    Menu Map Size added

--- Game/Assets/Scripts/MainMenu.cs
+++ Game/Assets/Scripts/MainMenu.cs
@@ -11,6 +11,9 @@ public class MainMenu : MonoBehaviour {
     private float buttonHeight;
     private float buttonWidth;
 
+    //New Game
+    private float levelSize = 2.0f;
+
 	// Use this for initialization
 	void Start () {
         screenWidth = Screen.width;
@@ -55,7 +58,39 @@ public class MainMenu : MonoBehaviour {
     private void NewGameMenu()
     {
         GUI.skin.label.alignment = TextAnchor.MiddleCenter;
-        GUI.Label(new Rect((screenWidth / 2) - (buttonWidth / 2), (screenHeight / 2) - (buttonHeight / 2) - 40, buttonWidth, buttonHeight), "New Game");
+        GUI.Label(new Rect((screenWidth / 2) - (buttonWidth / 2), (screenHeight / 2) - (buttonHeight / 2) - 100, buttonWidth, buttonHeight), "New Game");
+
+
+        GUI.Label(new Rect((screenWidth / 2) - (buttonWidth / 2) - (buttonWidth - 20), (screenHeight / 2) - (buttonHeight / 2) - 50, buttonWidth, buttonHeight), "Map Size");
+        levelSize = GUI.HorizontalSlider(new Rect((screenWidth / 2) - (buttonWidth / 2), (screenHeight / 2) - (buttonHeight / 2) - 40, buttonWidth, buttonHeight), levelSize, 1.0f, 5.0f);
+        levelSize = (int)levelSize;
+
+        string levelSizeString;
+
+        switch ((int)levelSize)
+        {
+            case 1:
+                levelSizeString = "Tiny";
+                break;
+            case 2:
+                levelSizeString = "Small";
+                break;
+            case 3:
+                levelSizeString = "Medium";
+                break;
+            case 4:
+                levelSizeString = "Large";
+                break;
+            case 5:
+                levelSizeString = "Huge";
+                break;
+            default:
+                levelSizeString = "Small";
+                break;
+        }
+
+        GUI.Label(new Rect((screenWidth / 2) - (buttonWidth / 2) + (buttonWidth + 20), (screenHeight / 2) - (buttonHeight / 2) - 50, buttonWidth, buttonHeight), levelSizeString);
+
 
         if (GUI.Button(new Rect((screenWidth / 2) - (buttonWidth / 2), (screenHeight / 2) - (buttonHeight / 2), buttonWidth, buttonHeight), "Start"))
         {
